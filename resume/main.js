1ffeb74d4944d2093c05e9c9054cac355bacbe9e
pfm-vond <pierre.vonderscher@gmail.com>
2019-12-26 19:31:53 +0100

    make lang choice dynamic based on available language.

--- resume/main.js
+++ resume/main.js
@@ -1,32 +1,35 @@
+var availableLang = [ "en", "fr" ];
+
 var app = new Vue({
-    el: '#resume',
-    i18n: Intl.Vue([
-        "location",
-        "organisation",
-        "studies",
-        "experiences",
-        "skills",
-        "various",
-        "abstracts",
-        "common",
-        "identity"
-      ],
-      [ "en", "fr" ]),
-    data: {
-      option: {
-        abstract: 'left',  
-        grade: 'hidden'
-      },
-      resume: Json.Fusion("content/", [
-        "studies",
-        "experiences",
-        "skills",
-        "various",
-        "abstracts",
-        "identity"
-      ])
+  el: '#resume',
+  i18n: Intl.Vue([
+      "location",
+      "organisation",
+      "studies",
+      "experiences",
+      "skills",
+      "various",
+      "abstracts",
+      "common",
+      "identity"
+    ],
+    availableLang),
+  data: {
+    option: {
+      availableLang,
+      abstract: 'left',  
+      grade: 'hidden',
     },
-    components: {
-      'resume': 'url:./components/resume.vue',
-    }
+    resume: Json.Fusion("content/", [
+      "studies",
+      "experiences",
+      "skills",
+      "various",
+      "abstracts",
+      "identity"
+    ])
+  },
+  components: {
+    'resume': 'url:./components/resume.vue',
+  }
 });
