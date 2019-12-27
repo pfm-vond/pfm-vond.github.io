f91422a468d26866dae8f889dfb747d652b259e9
pfm-vond <pierre.vonderscher@gmail.com>
2019-12-27 10:57:42 +0100

    simplify reading by removing v-bind in career.vue

--- resume/components/career.vue
+++ resume/components/career.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
     <simplelist type="abstract"
-      v-bind:option="option" 
-      v-bind:items="value.abstracts"></simplelist>
+      :option="option" 
+      :items="value.abstracts"></simplelist>
     <evaluationlist type="skills"
-      v-bind:option="option" 
-      v-bind:evaluations="value.skills"></evaluationlist>
+      :option="option" 
+      :evaluations="value.skills"></evaluationlist>
     <evaluationlist type="various"
-      v-bind:option="option" 
-      v-bind:evaluations="value.various"></evaluationlist>
+      :option="option" 
+      :evaluations="value.various"></evaluationlist>
     <cardlist type="experiences"
-      v-bind:option="option" 
-      v-bind:cards="value.experiences"></cardlist>
+      :option="option" 
+      :cards="value.experiences"></cardlist>
     <cardlist type="studies"
-      v-bind:option="option" 
-      v-bind:cards="value.studies"></cardlist>
+      :option="option" 
+      :cards="value.studies"></cardlist>
   </div>
 </template>
 
