0b024d60bdaff9cbfa43bdf5186a6278eb5f38dd
pfm-vond <pierre.vonderscher@gmail.com>
2019-12-28 10:52:44 +0100

    remove need for expressing all the index from translations. you can now just use the name of the table in resource and system will go over them all.

--- components/smart-paragraph.vue
+++ components/smart-paragraph.vue
@@ -5,6 +5,11 @@
         <smartline :option="option" :value="line">
       </p>
     </div>
+    <div v-else-if="isTranslationMultiline">
+      <p class="line" v-for="(line, index) in translations" :key="index">
+        <smartline :option="option" :value="value + '[' + index + ']'">
+      </p>
+    </div>
     <smartline v-else :option="option" :value="text"></smartline>
   </div>
 </template>
@@ -15,8 +20,10 @@
       data: function(){
         return {
           isMultiline: Array.isArray(this.value),
+          isTranslationMultiline: Array.isArray(this.$i18n.t(this.value)),
           text: this.value,
-          paragraph: this.value
+          paragraph: this.value,
+          translations: this.$i18n.t(this.value)
         }
       },
       components: {
